5a6bda12ab7b2a1319d9cdd30ec40dd960768bec
KorayGocmen <gocmen.koray@gmail.com>
2019-02-18 17:47:39 -0500

    scheduler server is implemented and worker can make requests to register and deregister on scheduler. Config methods are also implemented in both of them

---
+++ scheduler/workers.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"sync"
+)
+
+// workers holds the pointers to all the registered workers.
+// 		- key: workerID
+// 		- val: pointer to worker object.
+var (
+	workersMutex = &sync.Mutex{}
+	workers      = make(map[string]*worker)
+)
+
+// worker holds the information about registered workers
+// 		- id: uuid assigned when the worker first register.
+// 		- addr: workers network address, later used to create
+// 				grpc client to the worker.
+type worker struct {
+	id   string
+	addr string
+}
+
+// newWorker creates a new worker instance and adds
+// the new worker to the map.
+// Returns:
+// 		- string: worker id
+func newWorker(address string) string {
+	workersMutex.Lock()
+	defer workersMutex.Unlock()
+
+	// TODO: Change this back
+	// workerID := uuid.New().String()
+	workerID := "test_worker"
+	workers[workerID] = &worker{
+		id:   workerID,
+		addr: address,
+	}
+
+	return workerID
+}
+
+// delWorker removes the worker with the given id
+// from known workers map.
+func delWorker(id string) {
+	workersMutex.Lock()
+	defer workersMutex.Unlock()
+
+	if _, ok := workers[id]; ok {
+		delete(workers, id)
+	}
+}
---
+++ worker/worker.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var (
+	// workerID is the id assigned by the scheduler
+	// after registering on scheduler.
+	workerID string
+)
+
+func init() {
+	loadConfig()
+}
+
+// Entry point of the worker application.
+func main() {
+	go registerWorker()
+
+	sig := make(chan os.Signal)
+	signal.Notify(sig, syscall.SIGINT, syscall.SIGTERM)
+
+	for {
+		select {
+		case s := <-sig:
+			fatal(fmt.Sprintf("Signal (%d) received, stopping\n", s))
+		}
+	}
+}
+
+func fatal(message string) {
+	deregisterWorker()
+	log.Fatalln(message)
+}
