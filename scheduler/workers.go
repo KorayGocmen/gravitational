5c5b70d1bb44abf94816f2eff86dd78e194daf18
KorayGocmen <gocmen.koray@gmail.com>
2019-02-19 13:58:56 -0500

    addressing the comment regarding the heartbeat monitor between workers and scheduler

--- scheduler/workers.go
+++ scheduler/workers.go
@@ -4,6 +4,19 @@ import (
 	"sync"
 )
 
+// Currently the workers are kept in an in-memory model.
+// In a real production system there would be a need for a
+// persistent way to keep information about the online workers.
+// Such a persistent model would require two key features:
+// 		1. Keeping the data in a persistent database and loading this
+// 			 data to memory in the event of a scheduler crash/restart etc.
+// 		2. Continuous monitoring of the workers in the cluster by
+// 			the scheduler in order to be informed by a potential worker
+// 			crash. When workers are exiting normally, they deregister,
+// 			however in the event of a crash, the scheduler would be
+// 			unaware of the worker status. A heartbeat/ping monitor between
+// 			the scheduler and online workers would address this issue.
+
 // workers holds the pointers to all the registered workers.
 // 		- key: workerID
 // 		- val: pointer to worker object.
